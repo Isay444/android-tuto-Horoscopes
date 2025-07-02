93b729a7b467b0ae6922b77486d46206be3ea2c0
Isay444 <fareyisay3@gmail.com>
2025-07-02 13:34:59 -0600

    initial commit

---
+++ app/src/main/java/com/isa/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,22 @@
+package com.isa.horoscopoapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.isa.horoscopoapp.databinding.FragmentHoroscopeBinding
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
