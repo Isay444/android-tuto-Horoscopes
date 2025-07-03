95a64ad18a2a34fefbf15127752e02ab9ed83dfc
Isay444 <fareyisay3@gmail.com>
2025-07-03 15:34:41 -0600

    Dependencies hilt dagger kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -37,13 +40,18 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
 }
 
 dependencies {
-    val navVersion = "2.8.1"
+    val navVersion = "2.9.1"
     //Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    //DaggerHilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
