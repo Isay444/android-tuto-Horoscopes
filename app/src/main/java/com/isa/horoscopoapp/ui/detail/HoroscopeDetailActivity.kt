5821ef4be95492dcdd8d65fb89ce67d6928b2851
Isay444 <fareyisay3@gmail.com>
2025-07-03 15:40:26 -0600

    initial detail horoscope confg

---
+++ app/src/main/java/com/isa/horoscopoapp/ui/detail/HoroscopeDetailActivity.kt
@@ -0,0 +1,32 @@
+package com.isa.horoscopoapp.ui.detail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavArgs
+import androidx.navigation.navArgs
+import com.isa.horoscopoapp.R
+import com.isa.horoscopoapp.databinding.ActivityHoroscopeDetailBinding
+import com.isa.horoscopoapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HoroscopeDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHoroscopeDetailBinding
+    private val horoscopeDetailViewModel: HoroscopeDetailViewModel by viewModels()
+
+    private val args: HoroscopeDetailActivityArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        args.type
+    }
+}
